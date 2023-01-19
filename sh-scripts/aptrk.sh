f6c3f643947b502b4c3b329e1b60dced52609048
Rob Cochran <rob.cochran@apnic.com>
2023-01-19 19:34:17 +1000

    [nb] Commit

--- sh-scripts/aptrk.sh
+++ sh-scripts/aptrk.sh
@@ -9,12 +9,16 @@ append_pbpaste_to_track () {
         echo "Track already exists"
     else
         pbpaste | nb e tracks:1 #append to tracks.md
-        sed -i '' '/^$/d' ~/.nb/tracks/tracks.md #remove all blank lines from file
+        sed -i '' '/^$/d' ~/.nb/tracks/tracks.txt #remove all blank lines from file
         echo "Success"
     fi
 } 
 
+<<<<<<< HEAD:sh-scripts/aptrk.sh
 # Remove duplicate lines from a file, preserve original ordering, keep the first duplicate
 # cat -n stuff.txt | sort -uk2 | sort -nk1 | cut -f2-
 
 alias aptrk='append_pbpaste_to_track'
+=======
+alias aptrk='append_pbpaste_to_track'
+>>>>>>> 9181e7f ([nb] Commit):aptrk.sh
